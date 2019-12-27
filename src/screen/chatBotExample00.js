6d3857a7199c2938ec6f744887f4e876fe13b29b
vorti <vorti@vortiui-MacBookPro.local>
2019-12-27 17:32:32 +0900

    answer form making

--- src/screen/chatBotExample00.js
+++ src/screen/chatBotExample00.js
@@ -8,25 +8,28 @@ import {
     View,
     Animated,
     FlatList,
-    Button,
-    Text
+    Text,
+    TextInput,
+    TouchableOpacity,
+    ScrollView
 } from 'react-native';
 
 const chatTypes = {
     "01":{
         text : "Hello {@name}!\nWho are you going to {@location} with?",
         next : "02",
-        answerType : 1
+        answerType : 2,
+        select : ["Alone", "Couple", "Friends", "Familly"]
     },
     "02":{
         text : "Wow, very nice!\nTell me more about your familly!",
         next : "03",
-        answerType : 2
+        answerType : 3
     },
     "03":{
         text : "When are you planning to go?",
         next : "04",
-        answerType : 3
+        answerType : 4
     }
 }
 
@@ -34,6 +37,8 @@ export default function ChatBotExample00(){
 
     const [ animValue, setAnimValue] = useState(new Animated.Value(0))
     const [ chatList, setChatList] = useState([])
+    const [ answerContainerTrigger, setAnswerContainerTrigger ] = useState(false)
+    const [ question, setQuestion ] = useState(chatTypes["01"])
     const chatListViewHeight = animValue.interpolate({
         inputRange: [0, 1],
         outputRange: ['100%', '0%']
@@ -45,11 +50,11 @@ export default function ChatBotExample00(){
 
     useEffect(() => {
         if(chatList.length==0){
-            makeChatItem(chatTypes["01"])
+            addQuestion(question)
         }
     })
 
-    const setViewHeight = (val) => {
+    const setAnswerContainerHeight = (val) => {
         Animated.timing(
             animValue,
         {
@@ -59,8 +64,18 @@ export default function ChatBotExample00(){
         ).start();
     }
 
-    const makeChatItem = (val) => {
-        setChatList(chatList.concat([{text:val.text}]))
+    const addQuestion = (val) => {
+        setChatList(chatList.concat([{type: 'q', text:textChanger(val.text)}]))
+        setAnswerContainerTrigger(true)
+    }
+
+    const addAnswer = (val) => {
+        setChatList(chatList.concat([{type: 'a', text:val}]))
+        setAnswerContainerTrigger(false)
+    }
+
+    const textChanger = (text) => {
+        return text
     }
 
     return (
@@ -82,7 +97,12 @@ export default function ChatBotExample00(){
                         flex:1,
                     }}
                 >
-
+                    <AnswerContainer
+                        // answerType = {answerType}
+                        question = {question}
+                        addAnswer = {(value) => addAnswer(value)}
+                        setAnswerContainerHeight = {(value) => setAnswerContainerHeight(value)}
+                    />
                 </View>
             </Animated.View>
         </View>
@@ -90,23 +110,224 @@ export default function ChatBotExample00(){
 }
 
 function AChat(props){
-    console.log(props.chatData.text)
-    const re = /.*.}$/;
-    const chatText = re.exec(props.chatData.text)
-    console.log('re : ', chatText)
+    const chatPosition = props.chatData.type == 'q' ? "flex-start" : "flex-end"
+    const chatText = props.chatData.text
     return (
         <View
             style={{
                 width:'100%',
-                borderWidth:1,
+                borderBottomWidth:1,
+                alignItems:chatPosition
             }}
         >
-            {/* <Text>{chatText}</Text> */}
+            <Text>{chatText}</Text>
         </View>
     )
 }
 
-function textChanger(text){
-    const result = ""
-    return result
+function AnswerContainer(props){
+
+    const [ answerData, setAnswerData ] = useState("")
+
+    const answerTypes = () => {
+        switch(props.question.answerType){
+            case 1:
+                props.setAnswerContainerHeight(0.08)
+                return (
+                    <View
+                        style={{
+                            flex:1,
+                            flexDirection:'row'
+                        }}
+                    >
+                        <TextInput
+                            style={{flex:3}}
+                            onChangeText={answerData => setAnswerData(answerData)}
+                        />
+                        <TouchableOpacity
+                            style={{flex:1, alignItems:'center', justifyContent:'center'}}
+                            onPress={() => props.addAnswer(answerData)}
+                        >
+                            <Text>Button</Text>
+                        </TouchableOpacity>
+                    </View>
+                )
+            case 2:
+                props.setAnswerContainerHeight(0.3)
+                console.log(props.question.select)
+                return (
+                    <View
+                        style={{
+                            flex:1,
+                            flexDirection:'column'
+                        }}
+                    >
+                        <View
+                            style={{
+                                flex:1.5,
+                                paddingLeft:'5%',
+                                paddingRight:'5%'
+                            }}
+                        >
+                            <ScrollView
+                                horizontal={true}
+                            >
+                                {
+                                    props.question.select.map((content, index) => {
+                                        return (
+                                            <View
+                                                style={{
+                                                    width:150,
+                                                    height:"100%",
+                                                    alignItems:'center',
+                                                    justifyContent:'center'
+                                                }}
+                                            >
+                                                <TouchableOpacity
+                                                    style={{
+                                                        width:'80%',
+                                                        height:'80%',
+                                                        padding:5,
+                                                        borderWidth:1,
+                                                        borderRadius:25,
+                                                        borderColor:'#000AFF',
+                                                        alignItems:'center',
+                                                        justifyContent:'center'
+                                                    }}
+                                                >
+                                                    <Text
+                                                        style={{
+                                                            color:'#000AFF',
+                                                            fontSize:20,
+                                                        }}
+                                                    >
+                                                        {content}
+                                                    </Text>
+                                                </TouchableOpacity>
+                                            </View>
+                                        )
+                                    })
+                                }
+                            </ScrollView>
+                        </View>
+                        <View
+                            style={{
+                                flex:1,
+                                paddingLeft:'5%',
+                                paddingRight:'5%',
+                                alignItems:'center',
+                                justifyContent:'center'
+                            }}
+                        >
+                            <TextInput
+                                style={{
+                                    height:'100%',
+                                    width:'100%',
+                                    borderWidth:1,
+                                    borderStyle:'dotted',
+                                    borderRadius:5
+                                }}
+                                onChangeText={answerData => setAnswerData(answerData)}
+                            />
+                        </View>
+                        <View
+                            style={{
+                                flex:1.5,
+                                paddingLeft:'5%',
+                                paddingRight:'5%',
+                                flexDirection:'row'
+                            }}
+                        >
+                            <View
+                                style={{
+                                    flex:1,
+                                    alignItems:'flex-start',
+                                    justifyContent:'center'
+                                }}
+                            >
+                                <TouchableOpacity
+                                    style={{
+                                        width:'50%',
+                                        height:'80%',
+                                        padding:5,
+                                        borderWidth:1,
+                                        borderRadius:25,
+                                        borderColor:'#000AFF',
+                                        alignItems:'center',
+                                        justifyContent:'center'
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            color:'#000AFF',
+                                            fontSize:20,
+                                        }}
+                                    >
+                                        ↻
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                            <View
+                                style={{
+                                    flex:1,
+                                    alignItems:'flex-end',
+                                    justifyContent:'center'
+                                }}
+                            >
+                                <TouchableOpacity
+                                    style={{
+                                        width:'80%',
+                                        height:'80%',
+                                        padding:5,
+                                        borderWidth:1,
+                                        borderRadius:25,
+                                        borderColor:'#000AFF',
+                                        alignItems:'center',
+                                        justifyContent:'center'
+                                    }}
+                                >
+                                    <Text
+                                        style={{
+                                            color:'#000AFF',
+                                            fontSize:20,
+                                        }}
+                                    >
+                                        ✔
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                )
+            case 3:
+                return (
+                    <View
+                        style={{
+                            flex:1,
+                            backgroundColor:'blue'
+                        }}
+                    >
+                        
+                    </View>
+                )
+            default:
+                props.setAnswerContainerHeight(0)
+                return (
+                    <View>
+
+                    </View>
+                )
+        }
+    }
+
+    return (
+        <View
+            style={{
+                flex: 1,
+                borderTopWidth:1,
+            }}
+        >
+            {answerTypes()}
+        </View>
+    )
 }
--- src/screen/test.js
+++ src/screen/test.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import {
     View,
+    Animated,
     Button,
     Text,
     Alert
@@ -9,8 +10,37 @@ import {
   import LoadingChat from './loadingChat'
 
 export default (props) => {
+    const [ animValue, setAnimValue] = useState(new Animated.Value(0))
+    const backColor = animValue.interpolate({
+        inputRange: [0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1],
+        outputRange: ['#ffffff', '#ff4d4d', '#ffa366', '#ffa366', '#66ff66', '#6666ff', '#000080', '#6600cc', '#000000']
+    })
+    const changeColor = () => {
+        console.log(animValue)
+        const goVal = 0
+        if (animValue == 0){
+            goVal = 1
+        }
+
+        Animated.timing(
+            animValue,
+            {
+                toValue: 1,
+                duration: 10000,
+            }
+        ).start();
+    }
     return(
-        <View style={{flex:1}}>
-        </View>
+        <Animated.View
+            style={{
+                flex:1,
+                backgroundColor:backColor
+            }}
+        >
+            <Button
+                title = "Click"
+                onPress = {() => changeColor()}
+            />
+        </Animated.View>
     )
 }
